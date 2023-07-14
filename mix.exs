6f88cc1bad7500a6bca2612b0a4488023c0a9701
Patrick Smith <pgwsmith@gmail.com>
2023-07-14 12:54:28 +1000

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Orb.MixProject do
   def project do
     [
       app: :orb,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
