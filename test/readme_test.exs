c894067bdf591ed180c40ba56604521a30b8fa63
Patrick Smith <pgwsmith@gmail.com>
2023-08-04 11:20:26 +1000

    Add test to verify version in readme matches mix.exs
    See: https://twitter.com/royalicing/status/1687271985548820480

---
+++ test/readme_test.exs
@@ -0,0 +1,9 @@
+defmodule ReadmeTest do
+  use ExUnit.Case, async: true
+
+  test "version in readme matches mix.exs" do
+    readme_markdown = File.read!(Path.join(__DIR__, "../README.md"))
+    version = Mix.Project.config()[:version]
+    assert readme_markdown =~ ~s({:orb, "~> #{version}"})
+  end
+end
