0e42c822a01907eac63199c27fb915becf68c8cb
Patrick Smith <pgwsmith@gmail.com>
2023-11-29 12:38:51 +1100

    Use new global

--- test/examples/magic_numbers.exs
+++ test/examples/magic_numbers.exs
@@ -2,19 +2,17 @@ defmodule Examples.MagicNumbers do
   defmodule MobileThrottling do
     use Orb
 
-    I32.export_global(:readonly,
-      slow_3g_latency_ms: 2000,
-      slow_3g_download: 50_000,
-      slow_3g_upload: 50_000
-    )
+    # TODO: should it be called `const :export do` ?
+    global :export_readonly do
+      @slow_3g_latency_ms 2000
+      @slow_3g_download 50_000
+      @slow_3g_upload 50_000
+    end
 
-    I32.export_global(:readonly,
-      fast_3g_latency_ms: 563,
-      fast_3g_download: 180_000,
-      fast_3g_upload: 84_375
-    )
-
-    wasm do
+    global :export_readonly do
+      @fast_3g_latency_ms 563
+      @fast_3g_download 180_000
+      @fast_3g_upload 84_375
     end
   end
 end
