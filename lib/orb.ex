b9425e9cef95701c58fd7bf53abd9b9cc26b228c
Patrick Smith <pgwsmith@gmail.com>
2024-01-30 09:52:37 +1100

    Stub wasm output

--- lib/orb.ex
+++ lib/orb.ex
@@ -965,6 +965,25 @@ defmodule Orb do
     Orb.ToWat.to_wat(term, "") |> IO.chardata_to_string()
   end
 
+  @doc """
+  Convert Orb AST into WebAssembly binary format.
+  """
+  def to_wasm(term)
+
+  def to_wasm(term) when is_atom(term) do
+    term.__wasm_module__() |> to_wasm()
+  end
+
+  def to_wasm(term) when is_struct(term) do
+    <<"\0asm", 0x01000000::32>> <>
+      <<0x01, 0x05, 0x01, 0x60, 0x00, 0x01, 0x7F>> <>
+      <<0x03, 0x02, 0x01, 0x00>> <>
+      <<0x07, 0x0A, 0x01, 0x06, 0x61, 0x6E, 0x73, 0x77, 0x65, 0x72, 0x00, 0x00>> <>
+      <<0x0A, 0x06, 0x01, 0x04, 0x00, 0x41, 0x2A, 0x0B>>
+
+    # Orb.ToWat.to_wasm(term, "") |> IO.chardata_to_string()
+  end
+
   def __get_block_items(block) do
     case block do
       nil -> nil
---
+++ test/wasm_output_test.exs
@@ -0,0 +1,32 @@
+defmodule WasmOutputTest do
+  use ExUnit.Case, async: true
+
+  import Orb, only: [to_wasm: 1]
+
+  test "basic module" do
+    import Orb.DSL
+
+    defmodule Basic do
+      use Orb
+
+      defw answer(), I32 do
+        42
+      end
+    end
+
+    wasm_source = """
+    (module $Basic
+      (func $answer (export "answer") (result i32)
+        (i32.const 42)
+      )
+    )
+    """
+
+    assert <<"\0asm", 0x01000000::32>> <>
+             <<0x01, 0x05, 0x01, 0x60, 0x00, 0x01, 0x7F>> <>
+             <<0x03, 0x02, 0x01, 0x00>> <>
+             <<0x07, 0x0A, 0x01, 0x06, "answer", 0x00, 0x00>> <>
+             <<0x0A, 0x06, 0x01, 0x04, 0x00, 0x41, 0x2A, 0x0B>> =
+             to_wasm(Basic)
+  end
+end
