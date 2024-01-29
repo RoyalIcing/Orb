969bae2243df529cfe4a2523763ac70efaf2d999
Patrick Smith <pgwsmith@gmail.com>
2024-01-30 08:18:41 +1100

    Remove test with `wasm do`

--- test/dynamic_test.exs
+++ test/dynamic_test.exs
@@ -18,25 +18,7 @@ defmodule DynamicTest do
     end
   end
 
-  defmodule DynamicA do
-    use Orb
-
-    wasm do
-      case CurrentUser.get_type() do
-        :admin ->
-          func can_edit?(post_id: I32, author_id: I32), I32 do
-            1
-          end
-
-        :viewer ->
-          func can_edit?(post_id: I32, author_id: I32), I32 do
-            author_id === inline(do: CurrentUser.get_id())
-          end
-      end
-    end
-  end
-
-  defmodule DynamicB do
+  defmodule Dynamic do
     use Orb
 
     defw can_edit?(post_id: I32, author_id: I32), I32 do
@@ -57,29 +39,25 @@ defmodule DynamicTest do
   test "default user" do
     Process.put(:user_id, 123)
 
-    expected = """
-      (func $can_edit? (export "can_edit?") (param $post_id i32) (param $author_id i32) (result i32)
-        (i32.eq (local.get $author_id) (i32.const 123))
-      )
-    )
-    """
-
-    assert Orb.to_wat(DynamicA) =~ expected
-    assert Orb.to_wat(DynamicB) =~ expected
+    assert """
+           (module $Dynamic
+             (func $can_edit? (export "can_edit?") (param $post_id i32) (param $author_id i32) (result i32)
+               (i32.eq (local.get $author_id) (i32.const 123))
+             )
+           )
+           """ = Orb.to_wat(Dynamic)
   end
 
   test "admin user" do
     Process.put(:user_id, 123)
     Process.put(:user_type, :admin)
 
-    expected = """
-      (func $can_edit? (export "can_edit?") (param $post_id i32) (param $author_id i32) (result i32)
-        (i32.const 1)
-      )
-    )
-    """
-
-    assert Orb.to_wat(DynamicA) =~ expected
-    assert Orb.to_wat(DynamicB) =~ expected
+    assert """
+           (module $Dynamic
+             (func $can_edit? (export "can_edit?") (param $post_id i32) (param $author_id i32) (result i32)
+               (i32.const 1)
+             )
+           )
+           """ = Orb.to_wat(Dynamic)
   end
 end
