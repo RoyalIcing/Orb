b25215305625dbf75ac4d5ae9f0d528c596e3f72
Patrick Smith <pgwsmith@gmail.com>
2024-02-24 19:53:07 +1100

    Bump version

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Add `orb` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:orb, "~> 0.0.32"}
+    {:orb, "~> 0.0.33"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Orb.MixProject do
   def project do
     [
       app: :orb,
-      version: "0.0.32",
+      version: "0.0.33",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
