7844d56a2b0a593a8a1274b70a00b56bf95010b2
Patrick Smith <pgwsmith@gmail.com>
2024-03-18 13:14:50 +1100

    Test mut!

---
+++ test/mut_test.exs
@@ -0,0 +1,70 @@
+defmodule MutTest do
+  use ExUnit.Case, async: true
+
+  import Orb, only: [to_wat: 1]
+
+  test "with func parameter" do
+    import Orb.DSL
+
+    defmodule FuncParam do
+      use Orb
+
+      defw double(a: I32), I32, b: I32 do
+        with a_ref = mut!(a) do
+          a_ref.read * a_ref.read
+        end
+
+        mut!(b).write
+        mut!(b).read
+      end
+    end
+
+    wasm_source = """
+    (module $FuncParam
+      (func $double (export "double") (param $a i32) (result i32)
+        (local $b i32)
+        (i32.mul (local.get $a) (local.get $a))
+        (local.set $b)
+        (local.get $b)
+      )
+    )
+    """
+
+    assert to_wat(FuncParam) == wasm_source
+  end
+
+  test "with local variable" do
+    import Orb.DSL
+
+    defmodule LocalVar do
+      use Orb
+
+      defw double(), I32, a: I32, b: I32 do
+        a = 42
+
+        with a_ref = mut!(a) do
+          a_ref.read * a_ref.read
+        end
+
+        mut!(b).write
+        mut!(b).read
+      end
+    end
+
+    wasm_source = """
+    (module $LocalVar
+      (func $double (export "double") (result i32)
+        (local $a i32)
+        (local $b i32)
+        (i32.const 42)
+        (local.set $a)
+        (i32.mul (local.get $a) (local.get $a))
+        (local.set $b)
+        (local.get $b)
+      )
+    )
+    """
+
+    assert to_wat(LocalVar) == wasm_source
+  end
+end
