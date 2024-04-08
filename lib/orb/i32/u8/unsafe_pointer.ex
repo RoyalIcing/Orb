a28bd9cbd0e2050780d8025bc0ff9b0274c2ee48
Patrick Smith <pgwsmith@gmail.com>
2024-04-08 23:20:54 +1000

    Clarify actual alignment of I32.UnsafePointer

--- lib/orb/i32/u8/unsafe_pointer.ex
+++ lib/orb/i32/u8/unsafe_pointer.ex
@@ -5,8 +5,6 @@ defmodule Orb.I32.U8.UnsafePointer do
 
   alias Orb.{Memory, I32, VariableReference}
 
-  @behaviour Access
-
   with @behaviour Orb.CustomType do
     @impl Orb.CustomType
     def wasm_type, do: :i32
@@ -15,21 +13,23 @@ defmodule Orb.I32.U8.UnsafePointer do
     def load_instruction, do: :load8_u
   end
 
-  def alignment, do: 1
+  def memory_alignment, do: 1
 
-  @impl Access
-  def fetch(%VariableReference{} = var_ref, at!: offset) do
-    ast = Memory.load!(I32.U8, Orb.Numeric.Add.optimized(I32, var_ref, offset))
-    {:ok, ast}
-  end
+  with @behaviour Access do
+    @impl Access
+    def fetch(%VariableReference{} = var_ref, at!: offset) do
+      ast = Memory.load!(I32.U8, Orb.Numeric.Add.optimized(I32, var_ref, offset))
+      {:ok, ast}
+    end
 
-  @impl Access
-  def get_and_update(_data, _key, _function) do
-    raise UndefinedFunctionError, module: __MODULE__, function: :get_and_update, arity: 3
-  end
+    @impl Access
+    def get_and_update(_data, _key, _function) do
+      raise UndefinedFunctionError, module: __MODULE__, function: :get_and_update, arity: 3
+    end
 
-  @impl Access
-  def pop(_data, _key) do
-    raise UndefinedFunctionError, module: __MODULE__, function: :pop, arity: 2
+    @impl Access
+    def pop(_data, _key) do
+      raise UndefinedFunctionError, module: __MODULE__, function: :pop, arity: 2
+    end
   end
 end
--- lib/orb/i32/unsafe_pointer.ex
+++ lib/orb/i32/unsafe_pointer.ex
@@ -1,37 +1,39 @@
 defmodule Orb.I32.UnsafePointer do
   @moduledoc """
-  Custom `Orb.CustomType` for pointer to 32-bit integer in memory.
+  Custom `Orb.CustomType` for pointer to a 32-bit integer in memory.
+
+  It is aligned to 4 bytes or 32-bits, so you can only load/store at memory offsets divisible by 4.
   """
-
-  alias Orb.{Memory, I32, VariableReference}
-
-  @behaviour Access
 
   with @behaviour Orb.CustomType do
     @impl Orb.CustomType
     def wasm_type, do: :i32
   end
 
-  def alignment, do: 1
+  def memory_alignment, do: 4
 
-  @impl Access
-  def fetch(%VariableReference{} = var_ref, at!: offset) do
-    address =
-      offset
-      |> then(&Orb.Numeric.Multiply.optimized(I32, &1, 4))
-      |> then(&Orb.Numeric.Add.optimized(I32, &1, var_ref))
+  with @behaviour Access do
+    alias Orb.{Memory, I32, VariableReference}
 
-    ast = Memory.load!(I32, address)
-    {:ok, ast}
-  end
+    @impl Access
+    def fetch(%VariableReference{} = var_ref, at!: offset) do
+      address =
+        offset
+        |> then(&Orb.Numeric.Multiply.optimized(I32, &1, 4))
+        |> then(&Orb.Numeric.Add.optimized(I32, &1, var_ref))
 
-  @impl Access
-  def get_and_update(_data, _key, _function) do
-    raise UndefinedFunctionError, module: __MODULE__, function: :get_and_update, arity: 3
-  end
+      ast = Memory.load!(I32, address)
+      {:ok, ast}
+    end
 
-  @impl Access
-  def pop(_data, _key) do
-    raise UndefinedFunctionError, module: __MODULE__, function: :pop, arity: 2
+    @impl Access
+    def get_and_update(_data, _key, _function) do
+      raise UndefinedFunctionError, module: __MODULE__, function: :get_and_update, arity: 3
+    end
+
+    @impl Access
+    def pop(_data, _key) do
+      raise UndefinedFunctionError, module: __MODULE__, function: :pop, arity: 2
+    end
   end
 end
