7c7fef83f6857f31795d4eff7fa1551fe198413b
Patrick Smith <pgwsmith@gmail.com>
2024-06-13 20:58:48 +1000

    Extract string constants into own test

--- test/orb_test.exs
+++ test/orb_test.exs
@@ -144,36 +144,6 @@ defmodule OrbTest do
     end
   end
 
-  describe "string constants" do
-    test "assigns data" do
-      defmodule HTMLTypes do
-        use Orb
-
-        defw doctype(), I32 do
-          "<!doctype html>"
-        end
-
-        defw mime_type(), I32 do
-          "text/html"
-        end
-      end
-
-      assert to_wat(HTMLTypes) == """
-             (module $HTMLTypes
-               (; constants 26 bytes ;)
-               (data (i32.const 255) "<!doctype html>")
-               (data (i32.const 271) "text/html")
-               (func $doctype (export "doctype") (result i32)
-                 (i32.const 255)
-               )
-               (func $mime_type (export "mime_type") (result i32)
-                 (i32.const 271)
-               )
-             )
-             """
-    end
-  end
-
   describe "const/1" do
     test "assigns data" do
       defmodule ConstHTMLTypes do
