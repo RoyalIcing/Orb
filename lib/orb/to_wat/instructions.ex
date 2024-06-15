5e1f970cb63a0d58d10e8fb307a796670042e0d7
Patrick Smith <pgwsmith@gmail.com>
2024-06-15 15:08:59 +1000

    Remove {:raw_wat, source} support
    Instead custom instruction sequences have to implement Orb.ToWat & Orb.ToWasm

--- lib/orb/to_wat/instructions.ex
+++ lib/orb/to_wat/instructions.ex
@@ -16,18 +16,6 @@ defmodule Orb.ToWat.Instructions do
   # So should our default be 64-bit too?
   def do_wat(value, indent) when is_float(value), do: "#{indent}(f32.const #{value})"
 
-  # def do_wat({:raw_wat, source}, indent), do: "#{indent}#{source}"
-  def do_wat({:raw_wat, source}, indent) do
-    lines = String.split(source, "\n")
-
-    Enum.intersperse(
-      for line <- lines do
-        [indent, line]
-      end,
-      "\n"
-    )
-  end
-
   def do_wat(%_struct{} = value, indent) do
     # Protocol.assert_impl!(struct, Orb.ToWat)
 
