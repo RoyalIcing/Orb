369e7b4c234d2af0b38613c9debea2b640fea270
Patrick Smith <pgwsmith@gmail.com>
2024-11-10 17:51:38 +1100

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Orb.MixProject do
   def project do
     [
       app: :orb,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
