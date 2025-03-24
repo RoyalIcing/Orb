6acc819656a75da986edfae5a2f6f2ad9b4a6002
Patrick Smith <pgwsmith@gmail.com>
2025-03-24 16:30:16 +1100

    Let’s support Elixir 1.17 and 1.18

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Orb.MixProject do
     [
       app: :orb,
       version: "0.2.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "DSL for WebAssembly",
